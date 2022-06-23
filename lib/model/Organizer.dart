aeb982ab22f29e4f34ed2e45a43f35d08df41e7e
Symphony <49285362+symphony701@users.noreply.github.com>
2022-06-23 17:59:38 -0500

    More fixes

--- lib/model/Organizer.dart
+++ lib/model/Organizer.dart
@@ -1,8 +1,44 @@
-class Organizer{
-  final String name;
-  final String lastName;
-  final String img;
-  final String email;
-  final String password;
-  Organizer(this.name,this.lastName,this.img,this.email,this.password);
+class Organizer {
+  int? id;
+  String? firstName;
+  String? lastName;
+  String? email;
+  String? password;
+  String? logo;
+  String? description;
+  bool? verified;
+
+  Organizer(
+      {this.id,
+      this.firstName,
+      this.lastName,
+      this.email,
+      this.password,
+      this.logo,
+      this.description,
+      this.verified});
+
+  Organizer.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    firstName = json['firstName'];
+    lastName = json['lastName'];
+    email = json['email'];
+    password = json['password'];
+    logo = json['logo'];
+    description = json['description'];
+    verified = json['verified'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['id'] = id;
+    data['firstName'] = firstName;
+    data['lastName'] = lastName;
+    data['email'] = email;
+    data['password'] = password;
+    data['logo'] = logo;
+    data['description'] = description;
+    data['verified'] = verified;
+    return data;
+  }
 }
